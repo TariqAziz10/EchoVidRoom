e58fea88c5997ed51d64e15fbde453bb524d282e
TariqAziz10 <rockersss250@gmail.com>
2024-12-25 16:52:06 +0530

    manualchunk

--- frontend/videoCallApna/vite.config.js
+++ frontend/videoCallApna/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Split vendor libraries (e.g., React and React DOM) into a separate chunk
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
 })
